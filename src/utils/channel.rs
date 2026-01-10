30f30b241306ea320a2c04a142ebba9c1b72b62e
icceey <iceclock1037@gmail.com>
2026-01-10 16:49:21 +0800

    refactor: 将 BotChannelExt 实现从 Throttle<Bot> 移动到通用 Requester，以支持更广泛的请求器

--- src/utils/channel.rs
+++ src/utils/channel.rs
@@ -17,8 +17,8 @@
 
 use std::str::FromStr;
 
-use teloxide::adaptors::Throttle;
 use teloxide::prelude::*;
+use teloxide::requests::Requester;
 use teloxide::types::{ChatId, ChatMemberKind, ChatMemberStatus, Recipient};
 use tracing::info;
 
@@ -164,125 +164,10 @@ pub trait BotChannelExt {
     ) -> Result<ChatId, String>;
 }
 
-impl BotChannelExt for Bot {
-    async fn can_post_to_channel(&self, channel: &ChannelIdentifier) -> Result<bool, String> {
-        let me = match self.get_me().await {
-            Ok(me) => me,
-            Err(e) => {
-                tracing::error!("Failed to get bot info: {:#}", e);
-                return Err("获取机器人信息失败".to_string());
-            }
-        };
-        let bot_user_id = me.id;
-
-        let recipient = channel.to_recipient();
-        match self.get_chat_member(recipient.clone(), bot_user_id).await {
-            Ok(member) => {
-                let can_post = match &member.kind {
-                    ChatMemberKind::Administrator(admin) => {
-                        // Check if admin can post messages
-                        admin.can_post_messages
-                    }
-                    ChatMemberKind::Owner(_) => true,
-                    _ => false,
-                };
-
-                info!(
-                    "Bot {} in channel {:?}: status={:?}, can_post={}",
-                    bot_user_id,
-                    channel,
-                    member.status(),
-                    can_post
-                );
-
-                Ok(can_post)
-            }
-            Err(e) => {
-                tracing::error!(
-                    "Failed to get bot member status in channel {:?}: {:#}",
-                    channel,
-                    e
-                );
-                Err("无法获取机器人在频道中的状态".to_string())
-            }
-        }
-    }
-
-    async fn is_user_channel_admin(
-        &self,
-        channel: &ChannelIdentifier,
-        user_id: UserId,
-    ) -> Result<bool, String> {
-        let recipient = channel.to_recipient();
-        match self.get_chat_member(recipient, user_id).await {
-            Ok(member) => {
-                let is_admin = matches!(
-                    member.status(),
-                    ChatMemberStatus::Administrator | ChatMemberStatus::Owner
-                );
-
-                info!(
-                    "User {} in channel {:?}: status={:?}, is_admin={}",
-                    user_id,
-                    channel,
-                    member.status(),
-                    is_admin
-                );
-
-                Ok(is_admin)
-            }
-            Err(e) => {
-                tracing::error!(
-                    "Failed to get user {} member status in channel {:?}: {:#}",
-                    user_id,
-                    channel,
-                    e
-                );
-                Err("无法获取用户在频道中的状态".to_string())
-            }
-        }
-    }
-
-    async fn resolve_channel_id(&self, channel: &ChannelIdentifier) -> Result<ChatId, String> {
-        match channel {
-            ChannelIdentifier::Id(id) => Ok(*id),
-            ChannelIdentifier::Username(username) => {
-                // Fetch chat info to get the numeric ID
-                let recipient = Recipient::ChannelUsername(username.clone());
-                match self.get_chat(recipient).await {
-                    Ok(chat) => Ok(chat.id),
-                    Err(e) => {
-                        tracing::error!("Failed to get channel info for {:?}: {:#}", channel, e);
-                        Err("无法获取频道信息".to_string())
-                    }
-                }
-            }
-        }
-    }
-
-    async fn validate_channel_permissions(
-        &self,
-        channel: &ChannelIdentifier,
-        user_id: UserId,
-    ) -> Result<ChatId, String> {
-        // Check bot permissions first
-        let bot_can_post = self.can_post_to_channel(channel).await?;
-        if !bot_can_post {
-            return Err("机器人在该频道没有发送消息的权限".to_string());
-        }
-
-        // Check user is admin
-        let user_is_admin = self.is_user_channel_admin(channel, user_id).await?;
-        if !user_is_admin {
-            return Err("您不是该频道的管理员".to_string());
-        }
-
-        // Resolve to numeric ID for database storage
-        self.resolve_channel_id(channel).await
-    }
-}
-
-impl BotChannelExt for Throttle<Bot> {
+impl<R> BotChannelExt for R
+where
+    R: Requester + Sync,
+{
     async fn can_post_to_channel(&self, channel: &ChannelIdentifier) -> Result<bool, String> {
         let me = match self.get_me().await {
             Ok(me) => me,
